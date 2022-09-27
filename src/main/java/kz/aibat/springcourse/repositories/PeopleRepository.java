c957a5d3c76bc94ac60779467b2a7fea0e9a44ad
Tozhybayev Aibat <tozhybayev.aibat@gmail.com>
2022-09-27 11:03:17 +0600

    Initial commit

---
+++ src/main/java/kz/aibat/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package kz.aibat.springcourse.repositories;
+
+import kz.aibat.springcourse.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
